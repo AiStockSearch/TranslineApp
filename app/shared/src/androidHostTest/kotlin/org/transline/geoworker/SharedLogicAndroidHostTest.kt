8f356a12df66daff1678a5992cb86a368568978f
Slava <fintechmustreets@googlemail.com>
2026-07-21 12:23:50 +0500

    init kmp

---
+++ app/shared/src/androidHostTest/kotlin/org/transline/geoworker/SharedLogicAndroidHostTest.kt
@@ -0,0 +1,12 @@
+package org.transline.geoworker
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedLogicAndroidHostTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ app/shared/src/jvmTest/kotlin/org/transline/geoworker/SharedLogicDesktopTest.kt
@@ -0,0 +1,12 @@
+package org.transline.geoworker
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedLogicDesktopTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
